96bcfbcfef869b049271d80ddc5da79a9192b7e9
Daniel Joudat <danieljoudat@gmail.com>
2022-03-13 12:28:54 +0300

    some house cleaning
    Signed-off-by: Daniel Joudat <danieljoudat@gmail.com>

--- cmd/client-gen/web_generator.go
+++ cmd/client-gen/web_generator.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -12,7 +11,6 @@ import (
 
 	"github.com/fatih/camelcase"
 	"github.com/getkin/kin-openapi/openapi3"
-	"github.com/stoewer/go-strcase"
 )
 
 type webG struct {
@@ -134,74 +132,8 @@ func (w *webG) TopReadme(serviceName, examplesPath string, service service) {
 }
 
 func (w *webG) ExampleAndReadmeEdit(examplesPath, serviceName, endpoint, title string, service service, example example) {
-	// curl example
-	templ, err := template.New("cli" + serviceName + endpoint).Funcs(funcMap()).Parse(cliExampleTemplate)
-	if err != nil {
-		fmt.Println("Failed to unmarshal", err)
-		os.Exit(1)
-	}
-	b := bytes.Buffer{}
-	buf := bufio.NewWriter(&b)
-	err = templ.Execute(buf, map[string]interface{}{
-		"service":  service,
-		"example":  example,
-		"endpoint": endpoint,
-		"funcName": strcase.UpperCamelCase(title),
-	})
-
-	if err != nil {
-		fmt.Println("Failed to apply a parsed template to the specified data object", err)
-		os.Exit(1)
-	}
-
-	err = os.MkdirAll(filepath.Join(examplesPath, "cli", serviceName, endpoint), FOLDER_EXECUTE_PERMISSION)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	cliExampleFile := filepath.Join(examplesPath, "cli", serviceName, endpoint, title+".sh")
-	f, err := os.OpenFile(cliExampleFile, os.O_TRUNC|os.O_WRONLY|os.O_CREATE, FILE_EXECUTE_PERMISSION)
-	if err != nil {
-		fmt.Println("Failed to open schema file", err)
-		os.Exit(1)
-	}
-
-	buf.Flush()
-	_, err = f.Write(b.Bytes())
-	if err != nil {
-		fmt.Println("Failed to append to schema file", err)
-		os.Exit(1)
-	}
 }
 
 func schemaToWebExample(exampleJSON map[string]interface{}) string {
-	// type jsonObj map[string]interface{}
-	s := ""
-	for key, value := range exampleJSON {
-		// fmt.Println(value)
-		switch value.(type) {
-		case float64:
-			val := value.(float64)
-			s += "\t--" + key + "=" + fmt.Sprint(val) + " \\\n"
-		case int64:
-			val := value.(int64)
-			s += "\t--" + key + "=" + fmt.Sprint(val) + " \\\n"
-		case string:
-			s += "\t--" + key + "=" + "\"" + value.(string) + "\"" + " \\\n"
-		case interface{}:
-			bs, _ := json.MarshalIndent(value, "", "  ")
-			jsonList := strings.Split(string(bs), "\n")
-			s += "\t--" + key + "=" + "'" + jsonList[0] + "\n"
-			spacer := strings.Repeat(" ", 4+len(key))
-			for index, line := range jsonList[1:] {
-				if index == len(jsonList)-2 {
-					s += "\t" + spacer + line + "' \\\n"
-				} else {
-					s += "\t" + spacer + line + "\n"
-				}
-			}
-		}
-	}
-	return strings.TrimSuffix(s, "\\\n") + "\n"
+	return ""
 }
