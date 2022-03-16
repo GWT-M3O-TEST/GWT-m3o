9567c80c94a811b3bda6b69b2eca588bc93d3b3a
Daniel Joudat <danieljoudat@gmail.com>
2022-03-16 16:22:26 +0300

    fixes to m3o-web template
    Signed-off-by: Daniel Joudat <danieljoudat@gmail.com>

--- cmd/client-gen/web_template.go
+++ cmd/client-gen/web_template.go
@@ -12,7 +12,7 @@ const webHTMLServiceTemplate = `
 <!DOCTYPE html>
   <body>
     <div id="{{ $service.Name }}">
-      <form id="{{ untitle .endpoint }}" onsubmit="{{ $service.Name }}{{ .endpoint }}()">
+      <form id="{{ untitle .endpoint }}">
         <div>
             <label for="service"><b>{{ $service.Name }}</b></label>
             <input type="hidden" name="service" id="service" value="{{ $service.Name }}">
@@ -21,29 +21,29 @@ const webHTMLServiceTemplate = `
             <label for="endpoint"><b>{{ .endpoint }}</b></label>
             <input type="hidden" name="endpoint" id="endpoint" value="{{ .endpoint }}">
         </div>
-		<i>{{ .epdesc }}</i>
-		</br>
-		</br>
-		<label for="token">token </label>
+	      <i>{{ .epdesc }}</i>
+    		</br>
+    		</br>
+	      <label for="token">token </label>
         <div>
             <input name="token" id="token" placeholder="token">
         </div>
-		</br>
-		{{- range $property, $val := .properties }}
-		{{- if not (eq $val.Value.Type "object") }}
-		<label for="{{ $property }}">{{ $property }} </label>
+		    </br>
+    		{{- range $property, $val := .properties }}
+    		{{- if not (eq $val.Value.Type "object") }}
+	      <label for="{{ $property }}">{{ $property }} </label>
         <div>
             <input name="{{ $property }}" id="{{ $property }}" placeholder="{{ $val.Value.Description }}">
         </div>
-		{{- end }}
-		{{- if eq $val.Value.Type "object" }}
-		<label for="{{ $property }}">{{ $property }} </label>
+    		{{- end }}
+    		{{- if eq $val.Value.Type "object" }}
+	      <label for="{{ $property }}">{{ $property }} </label>
         <div>
             <textarea rows=5 cols=30 name="{{ $property }}" id="{{ $property }}" placeholder="{{ $val.Value.Description }}">{}</textarea>
         </div>
-		{{- end }}
-		{{- end }}
-        <button>Submit</button>
+    		{{- end }}
+    		{{- end }}
+        <button type="button" onclick="{{ $service.Name }}{{ .endpoint }}()">Submit</button>
       </form>
     </div>
     <div id="response"></div>
@@ -53,24 +53,24 @@ const webHTMLServiceTemplate = `
 `
 
 const webJSServiceTemplate = `
-{{ $service := .service -}}
+{{- $service := .service }}
 import Client from '../../client/index.js';
 
-function {{ $service.Name }}{{ .endpoint }}() {
-	var form = document.getElementById("{{ untitle .endpoint }}").value;
-	var token = form.elements["token"].value;
-	var service = form.elements["service"].value;
-	var endpoint = form.elements["endpoint"].value;
+window.{{ $service.Name }}{{ .endpoint }} = function () {
+	let form = document.getElementById("{{ untitle .endpoint }}").value;
+	let token = form.elements["token"].value;
+	let service = form.elements["service"].value;
+	let endpoint = form.elements["endpoint"].value;
 	{{- range $property, $val := .properties }}
-	var {{ $property }} = form.elements["{{ $property }}"].value;
+	let {{ $property }} = form.elements["{{ $property }}"].value;
 	{{- end }}
-	var obj = new Object();
+	let obj = new Object();
 	{{- range $property, $val := .properties }}
 	obj.{{ $property }} = {{ $property }};
 	{{ end }}
-	var request = JSON.stringify(obj);
+	let request = JSON.stringify(obj);
 
-	var m3o = new Client(token);
+	let m3o = new Client(token);
 
 	m3o.call(service, endpoint, request, function(response) {
 		document.getElementById("response").innerText = response;
